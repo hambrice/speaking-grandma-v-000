1facae4fce6e7179b65d22898d621444a7b79f82
Brad Hambrice <hambrice@uab.edu>
2018-01-04 07:06:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase)
   if phrase = "I LOVE YOU GRANDMA!"
-   return "I LOVE YOU TOO PUMPKIN!"phrase = phrase.upcase
-    return "NO, NOT SINCE 1938!"
+   return "I LOVE YOU TOO PUMPKIN!"
     elsif 
   else 
     return "HUH?! SPEAK UP, SONNY!"
